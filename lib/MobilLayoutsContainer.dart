d9f84141fb791ed85d57656cf49f375c65a2aa5b
GÜRKAY BAŞYİĞİT <gurkaybasyigit@jandarma.gov.tr>
2020-09-28 12:33:38 +0300

    add codes

---
+++ lib/MobilLayoutsContainer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      home: MyHomePage(title: 'Counter Home Page'),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
